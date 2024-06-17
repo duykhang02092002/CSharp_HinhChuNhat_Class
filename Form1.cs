4a9daf0f0e9b960ad8a4d6ccdcb3fae90baee165
ADMIN <ADMIN@LAB1-MAY20>
2024-06-17 14:15:04 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_HinhChuNhat_Class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Opacity = 100;
+        }
+
+        private void btnDientich_Click(object sender, EventArgs e)
+        {
+          Hình_chữ_nhật hcn = new Hình_chữ_nhật();
+            hcn.chieudai = Double.Parse(txtCao.Text);
+            hcn.chieurong = Double.Parse(txtRong.Text);
+            txtKetqua.Text=hcn.tinhdientich().ToString();
+        }
+
+        private void btnChuvi_Click(object sender, EventArgs e)
+        {
+            Hình_chữ_nhật hcn = new Hình_chữ_nhật();
+            hcn.chieudai = Double.Parse(txtCao.Text);
+            hcn.chieurong = Double.Parse(txtRong.Text);
+            txtKetqua.Text = hcn.tichchuvi().ToString();
+        }
+    }
+}
---
+++ Hình chữ nhật.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_HinhChuNhat_Class
+{
+    internal class Hình_chữ_nhật
+    {
+        public double chieudai;
+        public double chieurong;
+
+        public double tinhdientich()
+        {
+            return chieudai * chieurong;
+        }
+        public double tichchuvi()
+        {
+            return (chieudai+chieurong)*2;
+        }
+    }
+}
